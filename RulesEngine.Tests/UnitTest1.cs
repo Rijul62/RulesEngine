cb02468837920875fd97131527fcebc0a48eec99
Rijul Raveendran <rijulcit@gmail.com>
2020-07-02 16:11:01 +0530

    Added Test for TDD apprpoach with input and ouput parameters

--- RulesEngine.Tests/UnitTest1.cs
+++ RulesEngine.Tests/UnitTest1.cs
@@ -1,13 +1,24 @@
-using System;
 using Xunit;
 
 namespace RulesEngine.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(0, "None of the rules are applied")]
+        [InlineData(1, "Package slip generated")]
+        [InlineData(2, "Duplicate package slip generated")]
+        [InlineData(3, "Membership activated")]
+        [InlineData(4, "Email sent for activation/upgrade")]
+        [InlineData(5, " Free first aid video added")]
+        [InlineData(6, " Generated free commission agent")]
+        public void Given_ForDifferentPaymentTypes_When_RulesAreApplied_Then_TheExpectedOutputIsProcessed(int paymentType, string expectedResult)
         {
+            // Arrange
+
+            // Act
+
+            // Assert
 
         }
     }
