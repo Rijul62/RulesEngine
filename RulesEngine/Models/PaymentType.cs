1f77555fea928d99de1da4c7c42ad20236a8c735
Rijul Raveendran <rijulcit@gmail.com>
2020-07-02 16:21:33 +0530

    Added payment type enum

---
+++ RulesEngine/Models/PaymentType.cs
@@ -0,0 +1,12 @@
+﻿namespace RulesEngine.Models
+{
+    public enum PaymentType
+    {
+        NoRules = 0,
+        PhysicalProduct = 1,
+        Book = 2,
+        Membership = 3,
+        Upgrade = 4,
+        LearningSkiVideo = 5
+    }
+}
